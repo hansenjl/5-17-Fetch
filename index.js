233c8e87151481f7e055d3237c617f145b13da7f
hansenjl <jenn.leigh.hansen@gmail.com>
2021-05-26 20:39:25 -0700

    add styling

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const baseUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
 // EDIT ME ^^^^^^^^ 
+
+const searchForm = document.getElementById("search-form")
+const input = document.getElementById('search-input')
+const list = document.querySelector("#list")
